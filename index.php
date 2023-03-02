fe1d5b7612c4441d172c7750d576c6de5460a00e
Woedroe <118165188+WoutLar@users.noreply.github.com>
2023-03-02 13:45:28 +0100

    start of the site 1.0

--- index.php
+++ index.php
@@ -4,17 +4,28 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Iceland&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
-<body>
-    <div>sussy</div>
+<body>  
+
+    <header>
+            <div class="pomi">
+                <?php
+                echo "<p> <font color=red>Pomi </font>Grill & Sushi </p>";
+
+                ?>
+            </div>
+
+    </header>
 
     <?php
 
-    
 
-    ?>
-    
+    ?>    
 
 
 </body>
